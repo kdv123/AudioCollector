4f78d4efc5c7d1723d4c0905ac54caff17b09285
Robert Watling <rwatling0902@gmail.com>
2018-07-24 22:27:26 -0400

    Updated the SimpleAudioRecorder
    Attempted to clean up the SimpleAudioRecorder. Also added playback.

--- SimpleAudioRecorder.java
+++ SimpleAudioRecorder.java
@@ -1,7 +1,7 @@
 /*
- * Simple audio recorder with a few prompts from the console.
+ * Simple audio recorder with playback.
  * The program utilizes multi-threading.
- * Currently saving files in WAV format as buffering in audio input is a bit unclear
+ * Currently saving files in WAV format as buffering in audio input as raw binary is a "bit" unclear, but probably possible.
  */
 
 import java.io.File;
@@ -9,28 +9,64 @@ import java.io.IOException;
 import java.util.Scanner;
 import javax.sound.sampled.*;
 
+/**
+ * --------Miscellaneous Information--------
+ * 
+ * AudioFormat(AudioFormat.Encoding encoding, float sample rate, int sampleSizeInBits,
+ * int channels, int frameSizeInBytes, frameRate, boolean BigEndian (generally false))
+ * 
+ * Sample rate: samples / second
+ * Frame: 1 sample from each channel
+ * Frame size: sample size * channels
+ * Frame rate = frames / second
+ * 
+ * -----------------------------------------
+ */
 public class SimpleAudioRecorder {
+	float samplingRate;
+	File audioFile;
+	static AudioFormat myAF;
+	static Mixer mix;
+	static Clip testClip ;
+	static Scanner scan;
+	
+	public SimpleAudioRecorder() {
+		samplingRate = 44100;
+		audioFile = new File("test.wav");
+		myAF = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, samplingRate, 16, 1, 2, samplingRate, false);
+	}
+	
+	public SimpleAudioRecorder(float samplingRate, String fileName) {
+		if (samplingRate < 16000 || samplingRate > 44100)
+			samplingRate = 44100;
+		audioFile = new File(fileName + ".wav");
+		myAF = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, samplingRate, 16, 1, 2, samplingRate, false);
+	}
+	
 	public static void main (String[] args) {
-		float samplingRate = 44100;
+		SimpleAudioRecorder sar = new SimpleAudioRecorder();
+		
+		sar.record();
+		sar.playback();
+		
+		sar = new SimpleAudioRecorder(16000, "test2");
+		sar.record();
+		sar.playback();
+		
+		scan.close();
+		System.out.println("terminated");
+	} 
+
+	private void record() {
+		scan = new Scanner(System.in);
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, myAF);
 		
 		try {
-			/**
-			 * AudioFormat(AudioFormat.Encoding encoding, float sample rate, int sampleSizeInBits,
-			 * int channels, int frameSizeInBytes, frameRate, boolean BigEndian (generally false))
-			 * 
-			 * Sample rate: samples / second
-			 * Frame: 1 sample from each channel
-			 * Frame size: sample size * channels
-			 * Frame rate = frames / second
-			 */
-			AudioFormat myAF = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, samplingRate, 16, 1, 2, samplingRate, false);
-			DataLine.Info info = new DataLine.Info(TargetDataLine.class, myAF);
 			final TargetDataLine targetLine = (TargetDataLine) AudioSystem.getLine(info);
-			
-			Scanner scan = new Scanner(System.in);
+
 			System.out.println("Press <Enter> to begin recording");
 			scan.nextLine();
-			
+
 			targetLine.open();
 			System.out.println("Recording...");
 			targetLine.start();
@@ -44,7 +80,6 @@ public class SimpleAudioRecorder {
 				@Override
 				public void run() {
 					AudioInputStream ais = new AudioInputStream(targetLine);
-					File audioFile = new File("test.wav");
 					try {
 						AudioSystem.write(ais, AudioFileFormat.Type.WAVE, audioFile);
 					} catch (IOException e) {
@@ -57,18 +92,49 @@ public class SimpleAudioRecorder {
 			
 			System.out.println("Press <Enter> to stop");
 			scan.nextLine();
-			
+		
 			Thread.sleep(50);
 			targetLine.stop();
-			targetLine.close();
-			scan.close();
-			
-			System.out.println("terminated");
-			
+			targetLine.close();			
 		} catch (InterruptedException ie) {
 			ie.printStackTrace();
 		} catch (LineUnavailableException lue) {
 			lue.printStackTrace();
-		}	
+		}
+		
+		System.out.println("Recording has finished");
+	}
+	
+	private void playback() {
+		Mixer.Info[] mixInfos = AudioSystem.getMixerInfo();
+		mix = AudioSystem.getMixer(mixInfos[0]);	//Default system mixer
+		DataLine.Info playbackInfo = new DataLine.Info(Clip.class, myAF);
+		
+		System.out.println("Beginning playback...");
+		
+		try {
+			AudioInputStream playbackAudioStream = AudioSystem.getAudioInputStream(audioFile);
+			testClip = (Clip) mix.getLine(playbackInfo);
+			testClip.open(playbackAudioStream);
+		} catch (LineUnavailableException lue) {
+			lue.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (UnsupportedAudioFileException uafe) {
+			uafe.printStackTrace();
+		}
+		testClip.start();
+		
+		/*
+		 * Once testClip.start() is called, the audio will play but then the program will terminate before much of the audio is played.
+		 * Here is one way to "hack" the program into playing the whole clip.
+		 */
+		do {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		} while (testClip.isActive());
 	}
 }
